fa12a75f3c1049e756c5e3daeef443a2aa34c657
leeheisen <leeheisen@126.com>
2023-03-27 11:22:10 +0800

    add test scripts

---
+++ Python高级编程（第2版）/test.cs
@@ -0,0 +1,12 @@
+namespace Program{
+class Person {
+	public Name: str;
+	public Age: int;
+	public City: str;
+	public List<Pets> Pets: class Pets {
+	public Name: str;
+	public Type: str;
+};
+}
+
+}
